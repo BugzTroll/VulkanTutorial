739d8fab35ecfb0b940fd37e0afb2f586601f84d
Marie-Eve Dubé <mariedube2@gmail.com>
2019-11-02 19:50:13 -0400

    adding shaders

---
+++ vulcanTest/vulcanTest/shaders/compile.bat
@@ -0,0 +1,5 @@
+C:\Users\Bugz\Documents\vulcanProject\vulcanTest\dependencies\VulkanSDK\1.1.108.0\Bin32\glslc.exe shader.vert -o vert.spv
+
+C:\Users\Bugz\Documents\vulcanProject\vulcanTest\dependencies\VulkanSDK\1.1.108.0\Bin32\glslc.exe shader.frag -o frag.spv
+
+pause
